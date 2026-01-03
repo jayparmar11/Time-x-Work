cdbc66eec40ed72804728ed95d6fa307babed353
Jay Parmar <jaysparmar303@gmail.com>
2026-01-03 13:23:16 +0530

    fix error

--- preact/vite.config.ts
+++ preact/vite.config.ts
@@ -1,8 +1,14 @@
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+// import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), preact()],
+  base: './',
+  plugins: [
+    tailwindcss(),
+    preact(),
+    // viteSingleFile()
+  ]
 })
